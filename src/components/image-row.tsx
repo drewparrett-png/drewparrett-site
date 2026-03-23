c12b4d51b172531e08ef0223b55f381ac326b839
drewparrett-png <drew.parrett@gmail.com>
2026-03-23 18:59:49 -0400

    fix: use string props for ImageRow (MDX can't serialize arrays)

--- src/components/image-row.tsx
+++ src/components/image-row.tsx
@@ -1,19 +1,19 @@
 interface ImageRowProps {
-  images: Array<{ src: string; alt: string }>;
+  srcs: string;
+  alts?: string;
   maxItemWidth?: number;
 }
 
-export function ImageRow({ images, maxItemWidth = 200 }: ImageRowProps) {
+export function ImageRow({ srcs, alts, maxItemWidth = 200 }: ImageRowProps) {
+  const srcList = srcs.split(",").map((s) => s.trim());
+  const altList = alts ? alts.split(",").map((s) => s.trim()) : srcList.map(() => "");
+
   return (
     <div className="flex justify-center gap-4 my-6">
-      {images.map((img, i) => (
+      {srcList.map((src, i) => (
         <div key={i} style={{ width: maxItemWidth, flexShrink: 0 }}>
           {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img
-            src={img.src}
-            alt={img.alt}
-            className="w-full rounded-lg"
-          />
+          <img src={src} alt={altList[i] || ""} className="w-full rounded-lg" />
         </div>
       ))}
     </div>
