bdc669cd125c26b2dbc35fb5a6080e00cd4f8f67
drewparrett-png <drew.parrett@gmail.com>
2026-03-23 18:57:30 -0400

    fix: use props-based ImageRow to bypass MDX img override

--- src/components/image-row.tsx
+++ src/components/image-row.tsx
@@ -1,24 +1,21 @@
 interface ImageRowProps {
-  children: React.ReactNode;
-  maxItemWidth?: string;
+  images: Array<{ src: string; alt: string }>;
+  maxItemWidth?: number;
 }
 
-export function ImageRow({
-  children,
-  maxItemWidth = "200px",
-}: ImageRowProps) {
+export function ImageRow({ images, maxItemWidth = 200 }: ImageRowProps) {
   return (
-    <div
-      className="flex justify-center gap-4 my-6"
-      style={{ maxWidth: "100%" }}
-    >
-      {Array.isArray(children)
-        ? children.map((child, i) => (
-            <div key={i} style={{ width: maxItemWidth, flexShrink: 0 }}>
-              {child}
-            </div>
-          ))
-        : children}
+    <div className="flex justify-center gap-4 my-6">
+      {images.map((img, i) => (
+        <div key={i} style={{ width: maxItemWidth, flexShrink: 0 }}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img
+            src={img.src}
+            alt={img.alt}
+            className="w-full rounded-lg"
+          />
+        </div>
+      ))}
     </div>
   );
 }
